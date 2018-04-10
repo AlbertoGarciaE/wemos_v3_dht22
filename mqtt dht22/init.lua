107f56a34f3d617e5234056baeffb2d79ae3b3dc
AlbertoGarciaE <garcia.estevez.alberto@gmail.com>
2018-04-10 14:45:54 +0200

    Increase time to cancel init.lua load
    Increase to 30 seconds the time to cancel init.lua load by removing it from UART communication

--- mqtt dht22/init.lua
+++ mqtt dht22/init.lua
@@ -10,8 +10,8 @@ A 15 second delay has been added in case of error and enable abort.
 FileToExecute="mqtt_dht22.lua"
 
 -- Set timer to abort initialization of actual program
-print("You have 15 second to enter file.remove('init.lua') to abort")
-tmr.alarm(0, 15000, 0, function()
+print("You have 30 second to enter file.remove('init.lua') to abort")
+tmr.alarm(0, 30000, 0, function()
   print("Executing: ".. FileToExecute)
   dofile(FileToExecute)
 end)
